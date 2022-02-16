996202af71ff6f69a22eaee37f23a31365e1add5
Laurin Schofeld <l.schofeld@solarlux.de>
2022-02-16 09:56:59 +0100

    input validation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request, abort
 from models import List, Entry, User
 import json, controller
 
@@ -10,15 +10,18 @@ lists = []
 def hello_world():
     return "<h1>Dies ist die Todo-Listen API</h1>"
 
-@app.route('/list', methods=['POST'])
+@app.route('/list/', methods=['POST'])
 def add_list():
-    request_data = request.get_json(force=True)
-    list_name = request_data["name"]
-    list_entries = request_data["entries"]
-    new_list = List(list_name, list_entries)
-    lists.append(new_list)
-    ret = {"name": new_list.name, "entries": new_list.entries, "id": new_list.id}
-    return json.dumps(ret)
+    request_data = request.get_json()
+    if request_data is None or not List.can_create_from(request_data):
+        return abort(500, "It seems the data in your request did not satisfy the requrements to create a new Todo List.")
+    else:
+        new_todo_list = List(request_data.get("name"), request_data.get("entries"))
+        lists.append(new_todo_list)
+        return json.dumps(new_todo_list.to_dict())
+    
+    return abort(500)
+    
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -4,28 +4,61 @@
 import uuid, json
 
 class List:
-    def __init__(self, name, entries):
+    def __init__(self, name: str, entries: list):
         self.name = name
         self.entries = entries
         self.id = str(uuid.uuid4())
         
-    def list_completed(self):
-        for entry in self.entries:
-            if not entry.completed:
-                return False
+    @staticmethod
+    def can_create_from(todo_list_dict: dict) -> bool:
+        """
+        returns a boolean value to indicate whether the given dict can be turned into a valid (todo)List object
+        """
+        if not todo_list_dict.get("name"):
+            return False
+        elif not todo_list_dict.get("entries"):
+            return False
+        else:
+            for entry in todo_list_dict.get("entries"):
+                if not Entry.can_create_from(entry):
+                    return False
         return True
+            
+        
+    def to_dict(self) -> dict:
+        """
+        creates a dictionary from a (todo)List object
+        """
+        todo_list_dict = dict()
+        todo_list_dict["name"] = self.name
+        todo_list_dict["entries"] = self.entries
+        todo_list_dict["id"] = self.id
+        return todo_list_dict
         
         
 class User:
-    def __init__(self, user_name):
+    def __init__(self, user_name: str):
         self.user_name = user_name
         self.id = str(uuid.uuid4())
         
         
 class Entry:
-    def __init__(self, name, list_id, user_id, description = None):
+    def __init__(self, name: str, list_id: str, user_id: str, description = None):
         self.name = name
         self.id = str(uuid.uuid4())
         self.list_id = list_id
         self.user_id = user_id
         self.description = description if description is not None else ""
+        
+    @staticmethod
+    def can_create_from(entry_dict: dict) -> bool:
+        """
+        returns a boolean value to indicate whether the given dict can be turned into a valid Entry object
+        """
+        if not entry_dict.get("name"):
+            return False
+        elif not entry_dict.get("list_id"):
+            return False
+        elif not entry_dict.get("user_id"):
+            return False
+        return True
